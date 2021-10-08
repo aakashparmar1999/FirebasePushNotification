78b431dd1c11db1dd215453bd1ea9d3f2b39d933
aakashparmar <parmaraakash221199@gmail.com>
2021-10-08 14:20:23 +0530

    Firebase push notification

--- app/src/main/java/com/example/firebasepushnotification/MainActivity.kt
+++ app/src/main/java/com/example/firebasepushnotification/MainActivity.kt
@@ -9,8 +9,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "Main Activity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,6 +35,20 @@ class MainActivity : AppCompatActivity() {
                 Log.d("TAG", msg)
                 Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
             }
+        if (intent != null && intent.hasExtra("key1")) {
+            tv_hello.text = ""
+            for (key in intent.extras?.keySet()!!) {
+                try {
+                    Log.d(
+                        TAG,
+                        "onCreate: Key" + key.toString() + "DATA: " + intent.extras!!.getString(key)
+                    )
+                    tv_hello.append(intent.extras!!.getString(key))
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
 
+            }
+        }
     }
 }
