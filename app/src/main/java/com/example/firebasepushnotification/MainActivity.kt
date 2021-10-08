0bc130f23eac36047026ceab7ac0bd16e972bc02
aakashparmar <parmaraakash221199@gmail.com>
2021-10-08 17:09:04 +0530

    Firebase Cloud Messaging

--- app/src/main/java/com/example/firebasepushnotification/MainActivity.kt
+++ app/src/main/java/com/example/firebasepushnotification/MainActivity.kt
@@ -7,8 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.messaging.ktx.messaging
+import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,34 +20,55 @@ class MainActivity : AppCompatActivity() {
             val channel = NotificationChannel(
                 "MyNotification",
                 "MyNotification",
-                NotificationManager.IMPORTANCE_DEFAULT
+                NotificationManager.IMPORTANCE_HIGH
             )
             val manager = getSystemService(NotificationManager::class.java)
             manager.createNotificationChannel(channel)
         }
-        Firebase.messaging.subscribeToTopic("general")
-            .addOnCompleteListener { task ->
-                var msg = "msg_subscribed"
-                if (!task.isSuccessful) {
-                    msg = "msg_subscribe_failed"
-                }
-                Log.d("TAG", msg)
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-            }
-        if (intent != null && intent.hasExtra("key1")) {
-            tv_hello.text = ""
-            for (key in intent.extras?.keySet()!!) {
-                try {
-                    Log.d(
-                        TAG,
-                        "onCreate: Key" + key.toString() + "DATA: " + intent.extras!!.getString(key)
-                    )
-                    tv_hello.append(intent.extras!!.getString(key))
-                } catch (ex: Exception) {
-                    ex.printStackTrace()
+        bt_sub.setOnClickListener {
+
+            FirebaseMessaging.getInstance().subscribeToTopic("FCM")
+                .addOnCompleteListener { task ->
+                    var msg = "Subscribed"
+                    if (!task.isSuccessful) {
+                        msg = "Subscribe failed"
+                    }
+                    Log.d("TAG", msg)
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                 }
 
-            }
         }
+        bt_usub.setOnClickListener {
+
+            FirebaseMessaging.getInstance().unsubscribeFromTopic("FCM")
+                .addOnCompleteListener { task ->
+                    var msg = "Unsubscribed"
+                    if (!task.isSuccessful) {
+                        msg = "Unsubscribe failed"
+                    }
+                    Log.d("TAG", msg)
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                }
+
+        }
+        /*  if (intent != null && intent.hasExtra("key1")) {
+              tv_hello.text = ""
+              for (key in intent.extras?.keySet()!!) {
+                  try {
+                      Log.d(
+                          TAG,
+                          "onCreate: Key" + key.toString() + "DATA: " + intent.extras!!.getString(key)
+                      )
+                      //tv_hello.append(intent.extras!!.getString(key))
+                  } catch (ex: Exception) {
+                      ex.printStackTrace()
+                  }
+
+              }
+          } */
+    }
+
+    fun setText(data: String) {
+        tv_hello.text = data
     }
 }
--- app/src/main/java/com/example/firebasepushnotification/MyMessagingService.kt
+++ app/src/main/java/com/example/firebasepushnotification/MyMessagingService.kt
@@ -1,24 +1,53 @@
 package com.example.firebasepushnotification
 
+import android.graphics.Color
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyMessagingService : FirebaseMessagingService() {
+    val TAG: String = "MyTAG"
 
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
-        showNotification(p0.notification?.title!!, p0.notification!!.body!!)
+        Log.d(TAG, "onMessageReceived: called")
+        if (p0.notification != null) {
+            val title = p0.notification!!.title
+            val body = p0.notification!!.body
+            showNotification(title, body)
+        }
+        if (p0.data.isNotEmpty()) {
+            Log.d(TAG, "onMessageReceived: Data" + p0.data.toString())
+            for (key in p0.data.keys) {
+                Log.d(TAG, "onMessageReceived: Key:" + key + "Data: " + p0.data[key])
+                val ob = MainActivity()
+                ob.setText(p0.data.toString())
+            }
+        }
+
     }
 
-    private fun showNotification(title: String, message: String) {
+    private fun showNotification(title: String?, message: String?) {
+        Log.d(TAG, "showNotification: called")
         val builder = NotificationCompat.Builder(this, "MyNotification")
             .setContentTitle(title)
-            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setSmallIcon(R.drawable.ic_notifications)
             .setAutoCancel(true)
+            .setColor(Color.BLUE)
             .setContentText(message)
         val manager = NotificationManagerCompat.from(this)
         manager.notify(999, builder.build())
+    }
+
+    override fun onNewToken(p0: String) {
+        Log.d(TAG, "onNewToken: called")
+        super.onNewToken(p0)
+    }
+
+    override fun onDeletedMessages() {
+        Log.d(TAG, "onDeletedMessages: called")
+        super.onDeletedMessages()
     }
 }
